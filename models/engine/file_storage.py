82a7fc16f0db6e1233d2cb91934847f675f5a9c0
Ay-slim <ayooluwaadedipe@gmail.com>
2023-08-08 09:37:57 -0700

    Implement file storage and store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    Module to declare the file storage class
+    with its attributes and methods
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    A class to serialize instances to JSON
+    and deserialize JSONs to instnaces
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns all declared attributes of the instance
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Sets the obj dict as the value for key <obj class name>.id
+        """
+        if obj is not None:
+            obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            FileStorage.__objects[obj_key] = obj
+
+    def save(self):
+        """
+        Serializes the __objects attribute to the file path __file_path
+        """
+        tmp_dict = {}
+        for i, j in FileStorage.__objects.items():
+            tmp_dict[i] = j.to_dict()
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(tmp_dict, f)
+
+    def reload(self):
+        """
+        Deserialize the JSON file in __file_path into the __objects dict
+        if file exists
+        """
+        try:
+            with open(FileStorage.__file_path, "r") as f:
+                des_obj = json.load(f)
+                for i in des_obj.values():
+                    tmp_name = i.pop("__class__")
+                    self.new(eval(tmp_name)(**i))
+        except FileNotFoundError:
+            pass
